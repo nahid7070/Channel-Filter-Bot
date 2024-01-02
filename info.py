1f58ee09c424667e579c3bebd732bfe3bb84b781
nahid7070 <154741529+nahid7070@users.noreply.github.com>
2024-01-02 19:22:54 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = "26243378"
+API_HASH     = "33bf1090ef5a0c98dc46b189b6e4c435"
+BOT_TOKEN    = "6956586618:AAFOZ07US8grlhf9T4eUqrHIcwJnkiR2wFU"
+SESSION      = "BQGQcTIAua3Z0xiGELr4WXkHpq2mdML4tDkcwGBLVhXfLwVJQDGPShnlBOzNr3CkrYb81W_eXU2K_hcSBqVkwBs4p1Ae3MtL47jD3EMnPjyoMV45QeDZnhl2bKxa8aAGoNKJ0OqkwtFkTgVTziy8-KShAVtTqLZWLF7QZgkp_LDbSF2Wtwz1jB2gejEZg_EW3UDrgfHPRKSAL2tSbx5fY7xHCFmQa_pGYm18LeGvoSJpRif_y0pD34ZevB-DuNg77yBpWfOjmZKSNVAdFxR-7uw9g62IFNHGRIDHtQC3IPplVxzpeOWcX_xIf3GlsRJmvxzh0kHSInjano_DgSYNsvsSJBYPDwAAAAGdZCOnAA"
+DATABASE_URI = "mongodb+srv://nahidbd01210:nahidbd01210@cluster0.hqlcqqd.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1002088898902 
+ADMIN        = 6935552935
+CHANNEL      = "@ctgmovie270"
